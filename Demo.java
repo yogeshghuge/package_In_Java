2195bdb4aec4a4ea09e8832fde0d2df33d537aeb
Yogeshghughe1 <86541335+Yogeshghughe1@users.noreply.github.com>
2025-01-21 08:14:06 +0530

    Package in java

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import a.b.c.d;
+public class Demo
+{
+	public int a,b;
+	public void display()
+	{
+		System.out.println("a="+a+" b="+b);
+	}
+}
---
+++ MainClass.java
@@ -0,0 +1,12 @@
+import a.b.c.d.Demo;
+class MainClass
+{
+	public static void main(String args[])
+	{
+		Demo d1=new Demo();
+		d1.a=10;
+		d1.b=20;
+		d1.display();
+		a.b.c.d.Demo d2=new a.b.c.d.Demo();
+	}
+}
